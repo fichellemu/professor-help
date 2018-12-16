49b5908fb7c45b620dcb551607aefa4bfdc52a1a
Sck81 <45612716+Sck81@users.noreply.github.com>
2018-12-15 21:23:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,25 +5,34 @@ var app= express();
 var http=require('http');
 var mysql=require('mysql');
 
-
-var server= app.listen(process.env.PORT || 5000,function(){
+//process.env.PORT || 5000
+var server= app.listen(5000,function(){
     console.log("Server started on heuroku")
 });
+
 
 //setup sql connection
 //mysql://b0f34851335f6f:6917348c@us-cdbr-iron-east-01.cleardb.net/heroku_2d03868d61ad242?reconnect=true
 const con = mysql.createConnection({
-    host:"3306",
+    host:"us-cdbr-iron-east-01.cleardb.net",
     user:"b0f34851335f6f",
     password: "6917348c",
     database: "heroku_2d03868d61ad242"
 });
 
-
-app.get('/',function(req,res){
-    //res.send("hello this is the final project");
-    con.query("SELECT * FROM classes WHERE ClassSelectName=1", function(err,result){
-        res.send(result);
+//run our engine{items:result}
+app.set('view engine','ejs');
+var obj = {};
+app.get('/',function(req,res){   
+    con.query("SELECT * FROM classes WHERE ClassID='41'", function(err,result){
+        if (err){
+            console.log("ERROR");
+            return;
+        }
+        console.log("hello this is the final project");
+        console.log(result);
+        obj={items: result};
+        res.render("index",{items:result[0].ClassID});
+        console.log(result[0].ClassID);
     });
 });
-
