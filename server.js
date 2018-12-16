cfd871fecd9dddaf40ce7da08d55c5b930787615
Sck81 <45612716+Sck81@users.noreply.github.com>
2018-12-15 19:55:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,17 @@ var server= app.listen(process.env.PORT || 5000,function(){
     console.log("Server started on heuroku")
 });
 
+//setup sql connection
+//mysql://b0f34851335f6f:6917348c@us-cdbr-iron-east-01.cleardb.net/heroku_2d03868d61ad242?reconnect=true
+const con = mysql.createConnection({
+    host:"3306",
+    user:"b0f34851335f6f",
+    password: "6917348c",
+    database: "heroku_2d03868d61ad242"
+});
+
+
 app.get('/',function(req,res){
     res.send("hello this is the final project")
 });
+
